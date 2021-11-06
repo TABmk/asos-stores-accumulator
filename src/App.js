061c3c8f11b6777691dc635c90b5e714d6473548
TAB_mk <tabmk.contact@gmail.com>
2021-11-06 22:11:34 +0300

    promocode disc, cat select, price sort, extra info for goods

--- src/App.js
+++ src/App.js
@@ -6,39 +6,92 @@ import { objectToArray } from './helpers';
 import STORES from './stores.json';
 import './App.css';
 
-const url = 'https://www.asos.com/api/product/search/v2/categories/7368';
-const LIMIT = 100;
+const urlTemplate = 'https://www.asos.com/api/product/search/v2/categories/';
+const LIMIT = 200;
 
 function App() {
   const [items, setItems] = useState([]);
   const [page, setPage] = useState(0);
-  const [hasNext, setHasNext] = useState(false);
+  // const [hasNext, setHasNext] = useState(false);
+  const [rates, setRates] = useState({});
+  const [myCurr] = useState('RUB');
+  const [promocode, handlePromocode] = useState(0);
+  const [storeItems, setStoreItems] = useState(STORES.map(e => ({...e, count: 0, loaded: 0})));
 
-  const getFromStore = useCallback((i, offset = 0) => {
-    return new Promise(async (res, rej) => {
+  const params = new URLSearchParams(window.location.search);
+  const URL = params.get('cat');
+
+  const handleCategory = (e) => {
+    const { value } = e.target;
+
+    const cat = value.match(/cid=(\d+)/);
+
+    if (cat) {
+      window.location = '?cat=' + cat[1];
+    }
+  }
+
+  const sortItems = (sort) => {
+    let olditems = [...items];
+    
+    olditems = olditems.map(e => ({
+      ...e,
+      price: e.price.map(ee => {
+        const myCurrPrice = Math.floor(ee.value * rates[`${ee.currency}${myCurr}`]);
+        return {
+          ...ee,
+          myCurrPrice,
+          promoPrice: myCurrPrice * (1 - promocode/100),
+        }
+      }).sort((a, b) => a.myCurrPrice - b.myCurrPrice)
+    }));
+
+    if (sort === 'pricel') {
+      olditems = olditems.sort((a, b) => a.price[0].myCurrPrice - b.price[0].myCurrPrice)
+    }
+    if (sort === 'priceu') {
+      olditems = olditems.sort((a, b) => b.price[0].myCurrPrice - a.price[0].myCurrPrice)
+    }
+
+    setItems(olditems);
+  }
+
+  const getFromStore = useCallback(async (i, offset = 0) => {
       try {
-        const req = await fetch(url + `?country=${STORES[i].country}&currency=${STORES[i].currency}&lang=${STORES[i].lang}&limit=${LIMIT}&offset=${offset}&store=${STORES[i].store}`);
-        await res(req.json());
+        const req = await fetch(urlTemplate + URL + `?country=${STORES[i].country}&currency=${STORES[i].currency}&lang=${STORES[i].lang}&limit=${LIMIT}&offset=${offset}&store=${STORES[i].store}`);
+        return await req.json();
       }
       catch (err) {
         console.error(err);
-        rej(err)
+        throw new Error(err);
       }
-    })
   }, []);
 
   const getData = () => {
+    if (!URL) {
+      return;
+    }
+    fetch('http://getrates.su/')
+      .then(($) => $.json())
+      .then(setRates)
+
     const promises = [];
     for (let i = 0; i < STORES.length; i += 1) {
       promises.push(getFromStore(i, page * LIMIT));
     }
     if (promises.length !== 0) {
       const newItems = {};
-      let hasNextPage = false;
+      // let hasNextPage = false;
       Promise.all(promises).then((data) => {
         data.forEach((shop, i) => {
+          const _storeItems = [ ...storeItems ];
+          _storeItems[i].count = shop.itemCount;
+          _storeItems[i].loaded += shop.products.length;
+
+          setStoreItems(_storeItems);
           if (page * LIMIT + shop.products.length < shop.itemCount) {
-            hasNextPage = true;
+            setPage(page + 1);
+            // hasNextPage = true;
           }
           shop.products.forEach((item) => {
             if (!newItems[item.id]) {
@@ -55,40 +108,54 @@ function App() {
                 country: STORES[i].country,
                 value: item.price.current.value,
                 text: item.price.current.text,
+                currency: STORES[i].currency,
               })
             }
           });
         })
 
-        setHasNext(hasNextPage);
+        // setHasNext(hasNextPage);
         setItems([...items, ...objectToArray(newItems )]);
       });
     }
   }
 
   // TODO: do it better
-  useEffect(getData, [getFromStore, page]);
+  useEffect(getData, [getFromStore, URL, page]);
 
-  const onNextClick = () => {
-    setPage(page + 1);
-  }
+  // const onNextClick = () => {
+  //   setPage(page + 1);
+  // }
 
   return (
   <div>
-    <Grid fluid className="container">
+    Promocode % <input type="number" onChange={(e) => handlePromocode(e.target.value)} /> <br />
+    Category <input type="text" onChange={handleCategory} />
+    {
+      storeItems.map(e => (
+        <div key={e.store}>{e.store}: {e.count} items ({e.loaded} loaded)</div>
+      ))
+    }
+    Price: <button onClick={() => sortItems('pricel')}>🔼</button> <button onClick={() => sortItems('priceu')}>🔽</button>
+    {URL ? <Grid fluid className="container">
       <Row>
         {items.map((item) => (
-          <Col key={item.id} xs={12} md={6} lg={3}>
-            <ProductCard item={item} />
+          <Col key={item.id + Math.random()} xs={12} md={6} lg={3}>
+            <ProductCard
+              promocode={promocode}
+              rates={rates}
+              item={item}
+              myCurr={myCurr}
+            />
           </Col>
         ))}
       </Row>
-      {hasNext && (
+      {/* {hasNext && (
         <button onClick={onNextClick}>
           Next
         </button>
-      )}
-    </Grid>
+      )} */}
+    </Grid> : null }
   </div>
   );
 }
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,24 +1,48 @@
-const ProductCard = ({item}) => {
+import { useState } from 'react';
+
+const ProductCard = ({item, rates, myCurr, promocode}) => {
+  const [hidePrice, setHidePrice] = useState(true);
+
   return (
   <div className="prd">
     <img alt="" className="prd_img" src={'https://' + item.imageUrl} />
     <div className="prd_body">
       <div>{item.name}</div>
-      <table>
-        <tbody>
-          <tr>
-            <th>Store</th>
-            <th>Price</th>
-            <th>Your currency</th>
-          </tr>
-          {item.price?.map(price => (
-            <tr key={item.id + price.store} onClick={(() => window.open(`http://www.asos.com/${price.country}/prd/${item.id}?browseCountry=${price.country}&browseCurrency=${price.currency}`, '_blank'))}>
-              <th>{price.store}</th><th>{price.text}</th><th>XX</th>
+      <div className={hidePrice ? "tableContainer" : ''}>
+        <table>
+          <tbody>
+            <tr>
+              <th>🏠</th>
+              <th>💲</th>
+              <th>{myCurr}</th>
+              <th>🏷️</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+            {
+              item.price
+              ?.map(e => {
+                const myCurrPrice = Math.floor(e.value * rates[`${e.currency}${myCurr}`]);
+                return {
+                  ...e,
+                  myCurrPrice,
+                  promoPrice: myCurrPrice * (1 - promocode/100),
+                }
+              })
+              ?.sort((a, b) => a.myCurrPrice - b.myCurrPrice)
+              ?.map(price => (
+                <tr key={item.id + price.store} onClick={(() => window.open(`http://www.asos.com/${price.country}/prd/${item.id}?browseCountry=${price.country}&browseCurrency=${price.currency}`, '_blank'))}>
+                  <th>{price.store}</th><th>{price.text}</th><th>{price.myCurrPrice}</th><th>{Math.floor(price.promoPrice)}</th>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
+    {
+      item.price.length > 3
+       ? <div onClick={() => setHidePrice(!hidePrice)} className="priceSpoiler">{hidePrice ? '🔽🔽🔽' : '🔼🔼🔼'}</div>
+       : null
+    }
   </div>
 )};
 
